32e9f97d240a726fa6407ef03f05d6bbca6f0c6c
uned-official <51469498+uned-official@users.noreply.github.com>
2019-06-11 17:49:32 +0900

    20190611 1차 업데이트
    jquery 파일 업로드

---
+++ js/header_mobile.js
@@ -0,0 +1,9 @@
+$(function() {
+    /*nav-bar 버튼을 클릭했을때*/
+    $(".nav-bar").click(function () {
+        $("nav").toggleClass("on"); /*nav 영역이 보여진다.*/
+        $(".nav-bar").toggleClass("clicked"); /*nav-bar버튼이 펼쳐져있을때 모양이 'X'로 바뀌고, 아닐때는 '≡'로 바뀐다.*/
+    });
+    
+   
+});
