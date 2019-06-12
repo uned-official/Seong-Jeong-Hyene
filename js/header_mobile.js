4efd27aba609ec9f18567799c311d677b52db8e2
uned-official <51469498+uned-official@users.noreply.github.com>
2019-06-12 11:52:41 +0900

    20190612 design page img resize

--- js/header_mobile.js
+++ js/header_mobile.js
@@ -4,6 +4,4 @@ $(function() {
         $("nav").toggleClass("on"); /*nav 영역이 보여진다.*/
         $(".nav-bar").toggleClass("clicked"); /*nav-bar버튼이 펼쳐져있을때 모양이 'X'로 바뀌고, 아닐때는 '≡'로 바뀐다.*/
     });
-    
-   
 });
